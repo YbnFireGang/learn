5fa148c50187c67bd9b383e1de5932cb3814b760
wuxianwei <503158245@qq.com>
2018-09-01 05:46:57 -0700

    add QA FILE

---
+++ homework/1/js/dist/favorite.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var PraiseButton = function () {
+  function PraiseButton() {
+    var initFavorite = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+    _classCallCheck(this, PraiseButton);
+
+    this.favorite = initFavorite;
+  }
+
+  _createClass(PraiseButton, [{
+    key: 'addFavorite',
+    value: function addFavorite() {
+      this.favorite++;
+    }
+  }, {
+    key: 'getFavorite',
+    value: function getFavorite() {
+      return this.favorite;
+    }
+  }]);
+
+  return PraiseButton;
+}();
+
+var Thumb = function (_PraiseButton) {
+  _inherits(Thumb, _PraiseButton);
+
+  function Thumb() {
+    var initFavorite = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+    _classCallCheck(this, Thumb);
+
+    var _this = _possibleConstructorReturn(this, (Thumb.__proto__ || Object.getPrototypeOf(Thumb)).call(this, initFavorite));
+
+    _this.finger = 'Thumb';
+    return _this;
+  }
+
+  _createClass(Thumb, [{
+    key: 'getFingerType',
+    value: function getFingerType() {
+      return this.finger;
+    }
+  }]);
+
+  return Thumb;
+}(PraiseButton);
